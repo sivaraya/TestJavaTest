adededfaaddd72a6074f1ff6753b7e7604c58f7b
sivaraya <siva.rayapureddy@gmail.com>
2017-09-19 00:00:13 +0530

    Adding TestDemo Java File

---
+++ TestJavaTest/src/com/siva/test/TestDemo.java
@@ -0,0 +1,15 @@
+package com.siva.test;
+
+public class TestDemo {
+
+	void m1(){
+		System.out.println("Siva Rayapureddy");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TestDemo testDemo = new TestDemo();
+		testDemo.m1();
+
+	}
+
+}
